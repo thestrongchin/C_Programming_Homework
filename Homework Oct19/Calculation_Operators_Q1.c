bec17000be20e09dcfca0ed00664b2c62c4e26d6
thestrongchin <riverchen0510@gmail.com>
2024-10-24 20:40:59 -0700

    Calculation Operators Question 1
    In this push, I completed assignment 1 in the Calculation Operators file and got it correct.

---
+++ Homework Oct19/Calculation_Operators_Q1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a = 12;
+    int b = 4;
+    int multiplied = a*b;
+    int divided = a/b;
+    printf("multiplied number = %d \n", multiplied);
+    printf("divided number = %d \n", divided);
+    return 0;
+}
