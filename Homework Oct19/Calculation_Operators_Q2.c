bfb69afa569f70e3188023dfd99b85a54294e7d4
thestrongchin <riverchen0510@gmail.com>
2024-10-24 20:46:35 -0700

    Calculation Operators Question 2
    In the push, I complete assignment 2 in the Calculation Operators PDF and got it correct.

---
+++ Homework Oct19/Calculation_Operators_Q2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    char ch1 = 'E';
+    char ch2 = 'F';
+    int multiplied = ch1*ch2;
+    int divided = ch1/ch2;
+    printf("multiplied = %d \n", multiplied);
+    printf("divided = %d \n", divided);
+    return 0;
+}
