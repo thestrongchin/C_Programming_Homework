ee5dc6137bdbec1ca914ec3053febfee94dc8146
thestrongchin <riverchen0510@gmail.com>
2024-11-22 22:05:01 -0800

    Age thing
    In this push, I fixed some errors with my previous version of my age thing.

--- Age_Thing.c
+++ Age_Thing.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
 int main(){
-    int age;
-    int result = 100;
-    printf("Please enter your age");
-    result = scanf("%d", &age);
+    long long age;
+    long long result = 1;
+    printf("Please enter your age and I will tell you if you are an adult, child, teen, or about to die 😀😀😀:");
+    result = scanf("%lld", &age);
 
     if (result == 0){
-        printf("Sorry, you entered a character, enter a number \n");
+        printf("Sorry, you entered a character, enter a number 😭 \n");
         printf("END OF CODE, PLEASE RUN AGAIN \n");
         return 0;
     }
 
     else if (age<0){
-        printf("Are you not born? Enter a positive number as your age\n", age);
+        printf("Are you not born? Enter a POSITIVE NUMBER as your age 😡 \n");
+        printf("END OF CODE, PLEASE RUN AGAIN \n");
+        return 0;
+    }
+    else if (age > 150){
+        printf("How are you over 150 years old? I think you're about to die 💀💀💀 \n");
         printf("END OF CODE, PLEASE RUN AGAIN \n");
         return 0;
     }
  
     else if (age < 18){
-        printf("You are a child");
+        printf("You're a child 👶🏽");
         return 0;
     }
     
-    else if (age >= 18, age <= 25){
-        printf("You are a teen");
+    else if (age >= 18 && age <= 25){
+        printf("You're a teen 👦🏽");
         return 0;
     }
-    else (age > 25);
-        printf("You are an adult");
+    else if (age > 25)
+        printf("You're an adult 👨🏽 ");
+        return 0;
+    
+    if (age > 65){
+        printf("You're a senior 👴🏽");
         return 0;
     }
-
-
+}
