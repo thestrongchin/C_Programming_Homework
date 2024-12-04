fa5d2a9c1eac0aab338c6e52a18aeb2a928e9aff
thestrongchin <riverchen0510@gmail.com>
2024-12-04 10:54:32 -0800

    Homework
    Finished question 4

---
+++ Nov 30 Homework/Question_4.c
@@ -0,0 +1,24 @@
+// #include <stdio.h>
+// int main() {
+//     int age = 60;
+//     if(age < 18)
+//         printf("child\n");
+
+//     else if(18<= age <26)
+//     printf("teen \n");
+//     return 0;
+// }
+//I predict that the code will be an error or warning because in line 7, there is no logical operator. 
+//Also, in line 7, there needs to be a semicolon at the end, so it will be an error
+//Line 7 needs a && between = and age
+
+//Correction:
+#include <stdio.h>
+int main() {
+    int age = 60;
+    if(age < 18){
+        printf("child\n");
+    }else if(age >=18 && age <= 26)
+    printf("teen \n");
+    return 0;
+}
