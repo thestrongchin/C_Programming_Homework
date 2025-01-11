45b16e1d33c5f388ca2bdbf145b62b3fdde0a79b
River Chen <riverchen0510@gmail.com>
2025-01-11 15:46:32 -0800

    Capstone project
    edited some things in capstone project

--- Dec 14 Lecture/Example7.c
+++ Dec 14 Lecture/Example7.c
@@ -2,4 +2,4 @@
 int main(){ 
     printf("This code is on line number %d \n", __LINE__);
     return 0;
-}z
+}
--- Nov 30 Lecture/Example2.c
+++ Nov 30 Lecture/Example2.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 int main(){
     int count = 1;
-    int sum = 10;
 
-    while(count < 10){
-        sum = sum+count;
+    while(count){
         count++;
     }
     
-    printf("Count: %d \n", sum);
     return 0;
 }
