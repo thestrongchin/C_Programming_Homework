0a71689baff392c85c64c03a6105459f433dc8b3
River Chen <riverchen0510@gmail.com>
2025-03-27 13:57:51 -0700

    assignment 4
    finished assignment 4

--- Mar 15 Assignments/Assignment4.c
+++ Mar 15 Assignments/Assignment4.c
@@ -1,61 +1,37 @@
 #include <stdio.h>
 int main(){
     int percentage;
-    char level;
+    int level;
 
-    printf("Please enter a percentage to be converted to a grade and get feedback on your performance. ");
-    scanf(" %d", percentage);
+    printf("Please enter a percentage to be converted to a grade ");
+    scanf(" %d", &percentage);
 
-    if(percentage <= 100 || percentage >= 90){
-        level = 'A';
-    }
-
-    if(percentage <= 89 || percentage >= 80){
-        level = 'B';
-    }
-
-    if(percentage <= 79 || percentage >= 70){
-        level = 'C';
-    }
-
-    if(percentage <= 69 || percentage >= 60){
-        level = 'D';
-    }
+    level = percentage / 10;
     
-    if(percentage <= 59 || percentage >= 0){
-        level = 'F';
-    }
-
     switch(level){
-
-        case 'A':
-            printf("Excellent work! Your grade is: A. ");
+        case 6:
+            printf("Your grade is D.");
             break;
 
-        case 'B':
-            printf("Good work! Your grade is: B. ");
+        case 7: 
+            printf("Your grade is C.");
             break;
 
-        case 'C':
-            printf("Acceptable work. Your grade is: C. ");
+        case 8:
+            printf("Your grade is B");
             break;
 
-        case 'D':
-            printf("You can do better. Try harder next time. Your grade is: D. ");
-            break;
-        
-        case 'F':
-            printf("Think about how you failed. Try WAY harder next time. Your grade is F. ");
+        case 10:
+
+        case 9: 
+            printf("Your grade is A");
             break;
 
         default:
-            if(percentage < 100){
-                printf("Your percentage cannot be over 100%%. Program terminating.. .");
-            
-            }else{
-                printf("Your percentage cannot be under 0%%. Program terminating... ");
-            }
+            printf("Your grade is F");
             break;
     }
 
+return 0;
 }
+
--- Mar 15 Lecture/Example1.c
+++ Mar 15 Lecture/Example1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-    int day = 1; 
+    int day = 2; 
     switch (day) {
         case 1: 
             printf("Monday \n");
