d7a1a97b85638c9086632753615aef75901ca0be
River Chen <riverchen0510@gmail.com>
2025-05-07 22:12:33 -0700

    assignment 3

---
+++ Apr 26 Lecture/Assignment3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    int total = 0;
+
+    while (1) {
+        printf("Enter numbers to add: (enter a negative number to stop) ");
+        scanf("%d", &num);
+
+        if (num < 0) {
+            break;
+        }
+
+        total += num;
+    }
+
+    printf("The total sum is: %d\n", total);
+    return 0;
+}
