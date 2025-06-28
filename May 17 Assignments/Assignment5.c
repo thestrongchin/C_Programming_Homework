3f89f3d404ca7dec3ac763697d3d52b45b5a7ecb
River Chen <riverchen0510@gmail.com>
2025-06-28 14:57:46 -0700

    assignment 5
    finished assignment 5

--- May 17 Assignments/Assignment5.c
+++ May 17 Assignments/Assignment5.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 void printTri(int size, char character){
-    for (int i = 1; i <= size, i++) {
-        for (int j = 1; j <= i, j++){
-            printf("%c", character);
-        }
-    }
 
-    
-    
+    for (int i = 1; i <= size; i++) {
+
+        for (int j = 1; j <= i; j++){
+
+            printf("%c ", character);
+        }
+        printf("\n");
+    }
 }
 
 int main(void){
-    int triangleSize = 5;
+    int triangleSize = 10;
     char fillCharacter = '*';
 
     printTri(triangleSize, fillCharacter);
