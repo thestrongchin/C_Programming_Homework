01b4e16f879ae7f12e60829fc34ac2e51217a24b
River Chen <riverchen0510@gmail.com>
2025-07-11 13:56:28 -0700

    Organized again

--- Random Assignments/Age-Days_Calculator.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int age;
-    printf("Please enter your age");
-    scanf("%d", &age);
-    int age1 = age * 365.24;
-    printf("You have lived for %d days", age1);
-    return 0;
-}
--- Random/Tester.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-    int main() {
-    int a, b;
-    printf("Please enter two numbers:");
-    int result = scanf("%d %d ", &a, &b); // Trying to read two integers
-    printf("number of input: %d\n", result); // Print the return value
-    return 0;
-}
