037c16d8ab67e83b991c971921eeba4c8efb3248
River Chen <riverchen0510@gmail.com>
2025-07-21 19:12:57 +0800

    Update Capstone2.c

--- 99-Capstone Projects/Capstone2.c
+++ 99-Capstone Projects/Capstone2.c
@@ -1,3 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define DEBUG(msg) do { printf("LOG: %s \n", msg); } while(1)
+#define DEBUG
+#define MAX_ATTEMPTS = 10;
+
+int main() {
+    srand(time(NULL));
+
+    int num = rand() % 100 + 1;  
+    printf("Random number is: %d\n", num);  
+
+    return 0;
+}
+
+int get_user_guess(int guess){
+    while(1){
+    int attempts = MAX_ATTEMPTS;
+
+    if(guess >= 1){
+
+        if(guess < num){
+            printf("Too low!");
+            printf("Guesses remaining: %d \n", MAX_ATTEMPTS);
+        }
+    }
+
+        if(guess > num){
+            printf("Too high!");
+            printf("Guesses remaining: %d \n", MAX_ATTEMPTS);
+        }
+
+        if 
+
+    if(guess <= 0){
+        printf("invalid guess, please try again.");
+        printf("Guesses remaining: %d \n", MAX_ATTEMPTS);
+    }
+
+    }
+}
+
