89e76568ef41edfc4de32aa347fa9279093f354a
River Chen <riverchen0510@gmail.com>
2025-10-09 20:55:25 -0700

    finished question 3

---
+++ 17-Arrays/Assignments/Question3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int arr[5] = {1, 2, 3, 4, 5};
+    int total = arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
+
+    printf("Total is: %d", total);
+
+    return 0;
+}
