6df966cf6c0710d0a02155527cf837a8e2ed7221
River Chen <riverchen0510@gmail.com>
2025-10-17 22:45:49 -0700

    finished question 6

--- 17-Arrays/Assignments/Question6.c
+++ 17-Arrays/Assignments/Question6.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    int arr[6] = {10, 20, 30, 40, 50, 60};
-    printf("Non reversed array: %d, %d, %d, %d, %d, %d \n", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
-    printf("Reversed array: %d, %d, %d, %d, %d, %d", arr[5], arr[4], arr[3], arr[2], arr[1], arr[0]);
+    int arr1[4] = {2, 4, 6, 8};
+    int arr2[4] = {1, 0, 3, 6};
+    int arr3[4];
+    int size;
+
+    for(int i = 0; i < 4; i++){
+        arr3[i] = arr1[i] + arr2[i];
+    }
+
+    printf("Total: %d, %d, %d, %d", arr3[0], arr3[1], arr3[2], arr3[3]);
 
     return 0;
+
+    
+
+    
+
 
 }
