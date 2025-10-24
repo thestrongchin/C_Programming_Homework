c14f058ff7f2feda19c2b1bf8cf702c3772a2edd
River Chen <riverchen0510@gmail.com>
2025-10-23 23:11:56 -0700

    did most of question 5

---
+++ 17-Arrays/Assignments/Question5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int reverseArray(int arr[], int n){
+    for(int i = 0; i < size; i++){
+        n = arr[4], arr[3], arr[2], arr[1], arr[0];
+        return arr[i];
+    }
+
+
+}
+
+int main(){
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    reverseArray(arr, arr[4], arr[3], arr[2], arr[1], arr[0]);
+
+    for (int i = 0; i < 5; i++){
+        printf("Array: %d \n", arr[n]);
+    }
+
+    return 0;
+}
