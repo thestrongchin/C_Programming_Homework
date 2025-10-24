ed224234aa2529202fc2b303f7bc455a5e6cb507
River Chen <riverchen0510@gmail.com>
2025-10-24 12:51:04 -0700

    finished question 5

--- 17-Arrays/Assignments/Question5.c
+++ 17-Arrays/Assignments/Question5.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 int reverseArray(int arr[], int size){
-    for(int i = 0; i < size; i++){
-        n = arr[4], arr[3], arr[2], arr[1], arr[0];
-        return arr[i];
+    int temporary;
+
+    for(int i = 0; i < size / 2; i++){
+        temporary = arr[i];
+
+        arr[i] = arr[size - 1 - i]; // replaces current element with the opposite
+        arr[size - 1 -i] = temporary;
+        
     }
-
-
 }
 
 int main(){
@@ -15,7 +18,7 @@ int main(){
     reverseArray(arr, 5);
 
     for (int i = 0; i < 5; i++){
-        printf("Array: %d \n", arr[n]);
+        printf("Array: %d \n", arr[i]);
     }
 
     return 0;
