dccd3a206a60eb60c81f822844128d3162ec6aae
River Chen <riverchen0510@gmail.com>
2025-10-23 23:12:22 -0700

    did moset of question 5

--- 17-Arrays/Assignments/Question5.c
+++ 17-Arrays/Assignments/Question5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int reverseArray(int arr[], int n){
+int reverseArray(int arr[], int size){
     for(int i = 0; i < size; i++){
         n = arr[4], arr[3], arr[2], arr[1], arr[0];
         return arr[i];
@@ -12,7 +12,7 @@ int reverseArray(int arr[], int n){
 int main(){
     int arr[5] = {1, 2, 3, 4, 5};
 
-    reverseArray(arr, arr[4], arr[3], arr[2], arr[1], arr[0]);
+    reverseArray(arr, 5);
 
     for (int i = 0; i < 5; i++){
         printf("Array: %d \n", arr[n]);
