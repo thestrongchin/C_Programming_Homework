60567f9626410ad446e39ef2eb399b9734f9884b
River Chen <riverchen0510@gmail.com>
2025-11-08 21:58:59 -0800

    did question 8

--- 17-Arrays/Assignments/Question8.c
+++ 17-Arrays/Assignments/Question8.c
@@ -4,12 +4,13 @@ int main(){
     int arr[8] = {12, 45, 67, 23, 89, 34, 56, 78};
     float avg;
     int sum = 0;
+    int amount = sizeof(arr) / sizeof(arr[0]);
 
     for(int i = 0; i < 5; i++){
-        sum = sum + arr[i];
+        sum = sum + arr[i]; // adds all the elements and puts into sum
     }
 
-    avg = sum / 8;
+    avg = sum / amount;
 
     printf("The average is: %.2f", avg);
 
--- 17-Arrays/Assignments/Question9.c
+++ 17-Arrays/Assignments/Question9.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 int main(){
     long numbers[] = {1000, 2000, 3000, 4000, 1000};
+    int matching = numbers[0];
+    int temp = numbers[0];
 
-    int found = numbers[0];
+    for(int i = 0; i < 5; i++){
+        if(temp == numbers[i]){
+            int temp = matching;
 
-    for(int i = 0; i < 5; i++;){
-
-        if(numbers[i] == found){
-            found = numbers[i];
         }
-        found++;
     }
 
-    printf("The repeating number is: %d", numbers[i]);
+    printf("The repeating number is: %d", matching);
 
     return 0;
 }
