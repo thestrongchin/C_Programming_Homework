adde995e8182b09754b01a0983d7a23e3ac07ef5
River Chen <riverchen0510@gmail.com>
2025-11-08 16:19:06 -0800

    Finished question 6

--- 17-Arrays/Assignments/Question6.c
+++ 17-Arrays/Assignments/Question6.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int recieveArray(int arr[4], int arr2[4], int arr3[4]);
+int recieveArray(int arr[4], int arr2[4], int arr3[4]){
+    for(int i = 0; i < 4; i++){
+        arr3[i] = arr2[i] + arr[i];
+
+    }
+}
 
 int main(){
     int num1[4] = {2, 4, 6, 8};
@@ -10,9 +15,7 @@ int main(){
     recieveArray(num1, num2, num3);
 
     for(int i = 0; i < 4; i++){
-
-        num3[i] = num1[i] + num2[i];
-        printf("Array: %d \n", num3[i]);
+        printf("added: %d \n", num3[i]);
     }
 
     return 0;
