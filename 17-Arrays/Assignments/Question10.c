999725e22f65be4c8cef5b1f5a68033b63bd26cf
River Chen <riverchen0510@gmail.com>
2025-11-09 21:49:41 -0800

    finished question 10

--- 17-Arrays/Assignments/Question10.c
+++ 17-Arrays/Assignments/Question10.c
@@ -3,14 +3,16 @@
 
 int main(){
     bool boolArray[] = {1, 0, 1, 1, 0, 0};
-    int trues = 0;
+    int trueNum = 0;
 
     for(int i = 0; i < 6; i++){
+        if(boolArray[i] == true){
+            trueNum++;
 
-        if(boolArray[i] > 0){
-            trues++;
         }
     }
 
-    printf("There are %d true values", trues);
+    printf("There are %d true numbers", trueNum);
+
+    return 0;
 }
