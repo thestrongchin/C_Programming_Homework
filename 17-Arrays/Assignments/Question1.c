a47f30d4e27cffc30370ce455d405325d3cd5835
River Chen <riverchen0510@gmail.com>
2025-11-12 20:06:07 -0800

    update

--- 17-Arrays/Assignments/Question1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int arr[5] = {1, 2, 3, 4, 5};
-    printf("First array: %d Seccond array: %d", arr[0], arr[4]);
-
-    return 0;
-}
--- 17-Arrays/Assignments/Question7.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int arr[7] = {-2, 5, -1, 6, -4, -8, 6};
-    int largest1[];
-
-
-
-    return 0;
-}
