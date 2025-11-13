778768d38157dcfde89860d2843a761142ec3946
River Chen <riverchen0510@gmail.com>
2025-11-12 20:47:00 -0800

    did question 4

---
+++ 17-Arrays/Assignments/Question4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int arr[6];
+    int min = arr[0]
+    int max = 0;
+
+    for(int i = 0; i < 6; i++){
+
+        printf("Please enter element #%d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    for(int n = 0; n < 6; n++){
+        if(arr[0] )
+    }
+
+}
