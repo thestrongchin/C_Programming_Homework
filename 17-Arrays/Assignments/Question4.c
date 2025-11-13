863fc5d531487ce2ad0d8e8e824f262fac31d641
River Chen <riverchen0510@gmail.com>
2025-11-12 20:52:22 -0800

    issue

--- 17-Arrays/Assignments/Question4.c
+++ 17-Arrays/Assignments/Question4.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 int main(){
     int arr[6];
-    int min = arr[0]
-    int max = 0;
+    int min = arr[0];
+    int max = arr[0];
 
     for(int i = 0; i < 6; i++){
 
         printf("Please enter element #%d: ", i + 1);
-        scanf("%d", &arr[i]);
+        scanf("%d", &arr[n]);
     }
 
     for(int n = 0; n < 6; n++){
-        if(arr[0] )
+        if(min < arr[i]){
+            min = arr[n];
+        }
+
+        if(max > arr[n]){
+            max = arr[n];
+        }
     }
+
+    printf("Min: %d \n Max = %d \n", min, max);
+
+    return 0;
 
 }
